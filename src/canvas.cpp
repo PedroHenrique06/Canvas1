19a8d74b3271c3a076b35528611ec81fd24323ca
Pedro Henrique <90349947+PedroHenrique06@users.noreply.github.com>
2022-11-17 10:42:46 -0300

    Update canvas.cpp

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -19,28 +19,25 @@ namespace life {
       m_pixels[i+3] = 255;
     }
   }
-  /// Set the color of a pixel on the canvas.
+   /// Set the color of a pixel on the canvas.
   void Canvas::pixel(coord_t x, coord_t y, const Color& color){
     auto pos = y*m_width*4 + x*m_block_size*4;
 
-    m_pixels[pos] = color.channels[Color::R];
+    /*m_pixels[pos] = color.channels[Color::R];
     m_pixels[pos+1] = color.channels[Color::G];
     m_pixels[pos+2] = color.channels[Color::B];
-    m_pixels[pos+3] = 255; 
-
-
-/*  //lógica de um 5 x 20 para representar um pixel
-    //int count=0;
+    m_pixels[pos+3] = 255; */
+  
+  //Ainda não está pronto
   for(short i{0}; i<m_block_size; i++){
-    for(short j{0}; j<m_block_size; j++, pos+=4){
+    for(short j{0}; j<m_block_size; j++){
+      pos = (y+i)*m_width*4+(x+j)*m_block_size*4;
       m_pixels[pos] = color.channels[Color::R];
       m_pixels[pos+1] = color.channels[Color::G];
       m_pixels[pos+2] = color.channels[Color::B];
       m_pixels[pos+3] = 255;  
     }
-    pos = (y+i)*m_width*4 + x*m_block_size*4;
   }
-    //desce uma linha inteira e faz denovo */
     
   }
   /// Get the pixel color from the canvas.
